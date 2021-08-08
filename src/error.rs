686db1621fcb0a75d68f77d07e4d131d5a04766c
himanoa <matsunoappy@gmail.com>
2021-08-08 16:42:32 +0900

    Add  error

--- src/error.rs
+++ src/error.rs
@@ -2,6 +2,10 @@ use thiserror::Error;
 
 #[derive(Debug, PartialEq, Eq, Error)]
 pub enum ApplicationError {
+    #[error("action strategy error {msg:?}")]
+    ActionStrategyError {
+        msg: String
+    },
     #[error("unknown application error")]
     Unknown,
 }
