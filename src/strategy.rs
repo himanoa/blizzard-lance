1a4716bfe03e05d04a1acdf66d2833c0e642be7a
himanoa <matsunoappy@gmail.com>
2021-08-08 16:36:02 +0900

    Define strategy context

--- src/strategy.rs
+++ src/strategy.rs
@@ -1,11 +1,21 @@
-use crate::battle_action::ActorAction;
+use crate::{battle::BattleField, battle_action::{ActorAction, BattleFieldMutation}};
+
+pub struct ActionStrategyContext<'a> {
+    pub battle_field: &'a BattleField,
+    pub mutation: Box<dyn BattleFieldMutation>,
+}
+
+pub struct PreemptiveStrategyContext<'a> {
+    pub battle_field: &'a BattleField,
+    pub mutation: Box<dyn BattleFieldMutation>,
+}
 
 pub trait ActionStrategy {
-    fn execute(&self) -> Vec<ActorAction>;
+    fn execute(&self, ctx: &ActionStrategyContext) -> Vec<ActorAction>;
 }
 
 pub trait PreemptiveStrategy {
-    fn execute(&self) -> Vec<ActorAction>;
+    fn execute(&self, ctx: &PreemptiveStrategyContext) -> Vec<ActorAction>;
 }
 
 pub trait BattleStrategy
